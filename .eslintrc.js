fbd31d3d884c6e3d75f8b9d822c0fe190dbb968c
rdmclin2 <rdmclin2@gmail.com>
2023-05-08 18:31:50 +0800

    chore: 统一 prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
+const config = require('@umijs/lint/dist/config/eslint');
+
 module.exports = {
-  extends: require.resolve('@umijs/lint/dist/config/eslint'),
+  ...config,
+  rules: {
+    ...config.rules,
+    // 下述规则为判断后可以保留的部分
+    'array-callback-return': 'off',
+    'no-useless-escape': 'off',
+    'no-case-declarations': 'off',
+    '@typescript-eslint/no-use-before-define': [2, { functions: false }],
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
