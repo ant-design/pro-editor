9b3691c8319b1ee65286beb13e0b2e308e6ead5b
rdmclin2 <rdmclin2@gmail.com>
2023-05-09 10:13:51 +0800

    chore: mv icons to  DataFill

---
+++ src/DataFill/icons/index.d.ts
@@ -0,0 +1,7 @@
+export { default as LayoutMenuGroup } from '@alipay/prolayout-editor/src/models/icon/LayoutMenuGroup.js';
+export { default as FillCity } from './FillCity.js';
+export { default as FillCountry } from './FillCountry.js';
+export { default as FillMonth } from './FillMonth.js';
+export { default as FillNumber } from './FillNumber.js';
+export { default as FillText } from './FillText.js';
+export { default as FillWeek } from './FillWeek.js';
