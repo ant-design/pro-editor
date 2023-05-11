6cb2abd37412387d2db746072cd0082062b36692
rdmclin2 <rdmclin2@gmail.com>
2023-05-11 09:24:37 +0800

    chore: update snapshot

--- src/Highlight/demos/json.tsx
+++ src/Highlight/demos/json.tsx
@@ -7,8 +7,8 @@ import { Highlight } from '@ant-design/pro-editor';
 export default () => (
   <Highlight language="json">
     {{
-      name: 'TechUI',
-      description: '基于 Ant Design 的蚂蚁集团企业级 UI 资产库。',
+      name: 'ProComponents',
+      description: '基于 Ant Design 的 UI 资产库。',
       versions: ['1.0.0', '2.0.0', '2.5.1'],
     }}
   </Highlight>
