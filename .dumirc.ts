3cfbab914b5087720e3e111141e263c2d5959f3b
rdmclin2 <rdmclin2@gmail.com>
2023-05-11 10:55:06 +0800

    chore: code splitting

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   mfsu: false,
+  apiParser: {},
+  resolve: {
+    // 配置入口文件路径，API 解析将从这里开始
+    entryFile: './src/index.ts',
+  },
+  codeSplitting: {
+    jsStrategy: 'granularChunks',
+  },
   themeConfig: {
     hero: {
       'zh-CN': {
