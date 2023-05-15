1a4e0a8450247ff0f8bda01a74d41c18cfdbf5f2
arvinxx <arvinx@foxmail.com>
2023-05-15 20:29:08 +0800

    refactor: react-json-view 替换为 @textea/json-viewer

--- src/ProEditor/demos/controlledPresence.tsx
+++ src/ProEditor/demos/controlledPresence.tsx
@@ -1,13 +1,9 @@
 /**
  * iframe: 800
  */
-import {
-  ComponentAsset,
-  PresenceEditor,
-  ProEditor,
-} from '@ant-design/pro-editor';
+import { ComponentAsset, PresenceEditor, ProEditor } from '@ant-design/pro-editor';
+import { JsonViewer } from '@textea/json-viewer';
 import { useLocalStorageState } from 'ahooks';
-import ReactJson from 'react-json-view';
 import { Flexbox } from 'react-layout-kit';
 
 import { demoAssets } from '../../ComponentAsset/demoAssets';
@@ -21,9 +17,7 @@ const assets = new ComponentAsset({
 });
 
 export default () => {
-  const [presence, setPresence] = useLocalStorageState<PresenceEditor>(
-    'DEMO-PRO-EDITOR-PRESENCE',
-  );
+  const [presence, setPresence] = useLocalStorageState<PresenceEditor>('DEMO-PRO-EDITOR-PRESENCE');
   return (
     <Flexbox>
       <ProEditor
@@ -34,7 +28,7 @@ export default () => {
       />
       <Flexbox>
         当前视图状态：
-        <ReactJson src={presence} />
+        <JsonViewer value={presence} />
       </Flexbox>
     </Flexbox>
   );
