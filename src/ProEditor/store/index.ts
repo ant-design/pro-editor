ab25319b3b06815400fcbca2f926609033e456e6
大猫 <rdmclin2@gmail.com>
2023-05-24 21:02:43 +0800

    fix: lost dependency and theme site token inherit  (#7)
    * chore: add @mui/material
    
    * chore: pro-editor action
    
    * fix: item background token color and activate demoInheritSiteTheme
    
    ---------
    
    Co-authored-by: rdmclin2 <rdmclin2@gmial.com>

--- src/ProEditor/store/index.ts
+++ src/ProEditor/store/index.ts
@@ -2,9 +2,5 @@ export * from './createStore';
 export { EditorMode, TabKey } from './initialState';
 export type { PresenceEditor, ProEditorState } from './initialState';
 export * from './selectors';
-export type {
-  ProEditorAction,
-  ProEditorStore,
-  ProEditorStoreKey,
-} from './store';
+export type { ProEditorAction, ProEditorStore, ProEditorStoreKey } from './store';
 export * from './useInstance';
