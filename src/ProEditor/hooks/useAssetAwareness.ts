1c433f5950eeb62e4c09422409de0a18d9e59490
arvinxx <arvinx@foxmail.com>
2023-06-14 15:18:06 +0800

    :zap: perf: 优化画布相关操作实现，提升性能

--- src/ProEditor/hooks/useAssetAwareness.ts
+++ src/ProEditor/hooks/useAssetAwareness.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { useStore } from '../store';
 
-export const usePresenceAsset = <T>() => {
+export const useAssetAwareness = <T>() => {
   const presenceAsset = useStore((s) => s.assetAwareness) as T;
   const internalUpdatePresenceAsset = useStore((s) => s.internalUpdateAssetAwareness);
 
