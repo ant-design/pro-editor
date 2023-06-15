fa98010de7c055c0ee8b63c01b316491153501e0
arvinxx <arvinx@foxmail.com>
2023-06-15 11:24:31 +0800

    :bug: fix(pro-editor): 修正与 assetStore 的数据同步问题

--- src/ProEditor/components/AssetStoreUpdater/index.tsx
+++ src/ProEditor/components/AssetStoreUpdater/index.tsx
@@ -1,5 +1,6 @@
 import isEqual from 'fast-deep-equal';
 import { memo, useEffect } from 'react';
+import { storeApiSetState } from 'zustand-utils';
 import { shallow } from 'zustand/shallow';
 
 import { useProEditor } from '../../hooks/useProEditor';
@@ -19,39 +20,60 @@ const AssetStoreUpdater = memo(() => {
   );
   const assetStoreApi = useAssetStoreApi();
 
-  const setState = (state, action: any) => (assetStoreApi.setState as any)(state, false, action);
-
   useEffect(() => {
-    setState(instance, { type: '⏬ 注入 editor 方法', payload: Object.keys(instance) });
+    storeApiSetState(assetStoreApi, instance, false, {
+      type: '⏬ 注入 editor 方法',
+      payload: Object.keys(instance),
+    });
   }, []);
 
   // 将计算后的默认值传给面板
   // 用等式做一次优化，不然每次都会重新计算
-  const defaultConfig = useStore(
-    (s) =>
-      s.componentAsset.defaultConfig || configSelector(s.componentAsset.getDefaultConfig(s.mode)),
-    isEqual,
-  );
+  const defaultConfig = useStore((s) => {
+    let config;
+    if (s.componentAsset.defaultConfig) {
+      config = s.componentAsset.defaultConfig;
+    } else {
+      if (s.componentAsset.configSelector) {
+        config = s.componentAsset.configSelector(s.componentAsset.getDefaultConfig(s.mode));
+      } else {
+        config = s.componentAsset.getDefaultConfig(s.mode);
+      }
+    }
+
+    return config;
+  }, isEqual);
 
   const proEditorStoreApi = useStoreApi();
 
-  const syncState = (state) => setState(state, { type: '🔄 从 Editor 同步状态', payload: state });
-
-  // 用 defaultConfig 更新一次config
+  // 用 defaultConfig 更新 config
   useEffect(() => {
+    if (!!config) return;
     const state = { config: defaultConfig };
-    proEditorStoreApi.setState(state);
+    storeApiSetState(proEditorStoreApi, state, false, {
+      type: '⏬ 注入初始化 config',
+      payload: state,
+    });
     proEditorStoreApi.getState().yjsDoc.updateHistoryData(state);
 
-    setConfig(config, syncState);
-  }, []);
+    setConfig(defaultConfig, (state) => {
+      storeApiSetState(assetStoreApi, state, false, { type: '🔄 初始化状态', payload: state });
+    });
+  }, [defaultConfig]);
 
   // 将 proEditorStore 的 config 自动同步到  assetStore
   useEffect(() => {
-    const assetConfig = configSelector(assetStoreApi.getState());
-    if (isEqual(assetConfig, config)) return;
+    if (!!config) {
+      const assetConfig = configSelector(assetStoreApi.getState());
+      if (isEqual(assetConfig, config)) return;
 
-    setConfig(config, syncState);
+      setConfig(config, (state) => {
+        storeApiSetState(assetStoreApi, state, false, {
+          type: '🔄 同步 Editor 状态',
+          payload: state,
+        });
+      });
+    }
   }, [config]);
 
   return null;
