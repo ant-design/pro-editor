520ce62dc373fc7a3f3fc1d806805ed5b2f5b8ba
arvinxx <arvinx@foxmail.com>
2023-06-27 12:51:23 +0800

    :memo: docs: 优化 SortableList demo 和文档

--- src/SortableList/demos/compact.tsx
+++ src/SortableList/demos/compact.tsx
@@ -1,3 +1,7 @@
+/**
+ * title: 紧凑模式
+ * description: 提供 `compact` 配置型开启紧凑模式，开启后把手和操作项会浮动在内容区域内部，需用户自行调整间距。
+ */
 import { SortableList } from '@ant-design/pro-editor';
 import ItemRender from './_ItemRender';
 import { INIT_VALUES, SchemaItem } from './data';
