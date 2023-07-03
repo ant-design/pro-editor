9a5b678bd1be217133c409cf11ac42e79d227de6
arvinxx <arvinx@foxmail.com>
2023-07-03 10:17:28 +0800

    :rotating_light: ci: fix lint

--- src/SortableList/demos/CustomRender.tsx
+++ src/SortableList/demos/CustomRender.tsx
@@ -1,5 +1,5 @@
 import { SortableList } from '@ant-design/pro-editor';
-import { Badge, Button } from 'antd';
+import { Button } from 'antd';
 import { useTheme } from 'antd-style';
 import { useState } from 'react';
 import { Flexbox } from 'react-layout-kit';
@@ -50,57 +50,57 @@ const Demo = () => {
         </Button>
       </Flexbox>
 
-      <SortableList<Item[]>
+      <SortableList<Item>
         value={list}
         onChange={setList}
-        getItemStyles={() => ({ padding: '16px' })}
-        renderItem={(item: Item, { onRemove, onAddItem, index }) => {
-          return (
-            <Flexbox horizontal width={'100%'} distribution={'space-between'} gap={12}>
-              <Flexbox horizontal gap={8}>
-                <div>
-                  <Badge count={item.id} />
-                </div>
-                <div>{item.text}</div>
-              </Flexbox>
-              <Flexbox
-                horizontal // 由于拖拽事件是通过监听 onMouseDown 来识别用户动作
-                // 因此针对相关用户操作，需要终止 onMouseDown 的冒泡行为
-                onMouseDown={(e) => {
-                  e.stopPropagation();
-                }}
-              >
-                <Button
-                  size={'small'}
-                  type={'text'}
-                  onClick={() => {
-                    onAddItem(index, {
-                      id: Math.ceil(Math.random() * 100000).toString(16),
-                      text: 'new',
-                    });
-                  }}
-                >
-                  上方
-                </Button>
-                <Button
-                  size={'small'}
-                  type={'text'}
-                  onClick={() => {
-                    onAddItem(index + 1, {
-                      id: Math.ceil(Math.random() * 1000).toString(16),
-                      text: 'new',
-                    });
-                  }}
-                >
-                  下方
-                </Button>
-                <Button size={'small'} danger type={'text'} onClick={onRemove}>
-                  删除
-                </Button>
-              </Flexbox>
-            </Flexbox>
-          );
-        }}
+        // getItemStyles={() => ({ padding: '16px' })}
+        // renderItem={(item: Item, { onRemove, onAddItem, index }) => {
+        //   return (
+        //     <Flexbox horizontal width={'100%'} distribution={'space-between'} gap={12}>
+        //       <Flexbox horizontal gap={8}>
+        //         <div>
+        //           <Badge count={item.id} />
+        //         </div>
+        //         <div>{item.text}</div>
+        //       </Flexbox>
+        //       <Flexbox
+        //         horizontal // 由于拖拽事件是通过监听 onMouseDown 来识别用户动作
+        //         // 因此针对相关用户操作，需要终止 onMouseDown 的冒泡行为
+        //         onMouseDown={(e) => {
+        //           e.stopPropagation();
+        //         }}
+        //       >
+        //         <Button
+        //           size={'small'}
+        //           type={'text'}
+        //           onClick={() => {
+        //             onAddItem(index, {
+        //               id: Math.ceil(Math.random() * 100000).toString(16),
+        //               text: 'new',
+        //             });
+        //           }}
+        //         >
+        //           上方
+        //         </Button>
+        //         <Button
+        //           size={'small'}
+        //           type={'text'}
+        //           onClick={() => {
+        //             onAddItem(index + 1, {
+        //               id: Math.ceil(Math.random() * 1000).toString(16),
+        //               text: 'new',
+        //             });
+        //           }}
+        //         >
+        //           下方
+        //         </Button>
+        //         <Button size={'small'} danger type={'text'} onClick={onRemove}>
+        //           删除
+        //         </Button>
+        //       </Flexbox>
+        //     </Flexbox>
+        //   );
+        // }}
       />
     </Flexbox>
   );
