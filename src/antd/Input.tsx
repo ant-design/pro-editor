e1c7d4fc8e739aa9c6073274c7ca4ebd43c10f04
Arvin Xu <arvinx@foxmail.com>
2023-07-03 09:49:28 +0800

    :bug: fix: 优化 Input 和 InputNumber 组件的受控变更逻辑 (#45)
    * :bug: fix: 优化 Input、InputNumber 实现，只在 blur 和回车时onChange
    
    * :camera_flash: test: 更新快照

--- src/antd/Input.tsx
+++ src/antd/Input.tsx
@@ -1,21 +1,65 @@
 import { InputRef, InputProps as Props, Input as _Input } from 'antd';
-import { forwardRef, useRef } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 
 import { ConfigProvider } from '../ConfigProvider';
 import { createStyles } from '../theme';
-
-export type InputProps = Props;
 
 const useStyles = createStyles(
   ({ stylish, css }) => css`
     ${stylish.controlContainer}
   `,
 );
-
+export interface InputProps extends Omit<Props, 'onChange' | 'value' | 'onAbort'> {
+  /**
+   * 输入框变化回调函数
+   * @param value - 输入框的值
+   */
+  onChange?: (value: string) => void;
+  /**
+   * 输入框值变化前回调函数
+   * @param value - 输入框的值
+   */
+  onValueChanging?: (value: string) => void;
+  /**
+   * 输入框的值
+   */
+  value?: string;
+  defaultValue?: string;
+}
 export const Input = forwardRef<InputRef, InputProps>(
-  ({ className, onCompositionStart, onCompositionEnd, ...props }, ref) => {
+  (
+    {
+      className,
+      defaultValue,
+      onChange,
+      value,
+      onCompositionStart,
+      onCompositionEnd,
+      onBlur,
+      onFocus,
+      onValueChanging,
+      ...props
+    },
+    ref,
+  ) => {
     const { styles, cx } = useStyles();
     const compositionRef = useRef(false);
+
+    const [input, setInput] = useState(value ?? defaultValue);
+    const valueRef = useRef(defaultValue || value);
+
+    useEffect(() => {
+      if (typeof value !== 'undefined') {
+        setInput(value);
+      }
+    }, [value]);
+
+    const triggerUpdate = () => {
+      if (input !== valueRef.current) {
+        onChange?.(input);
+        valueRef.current = input;
+      }
+    };
 
     return (
       <ConfigProvider>
@@ -23,8 +67,21 @@ export const Input = forwardRef<InputRef, InputProps>(
           {...props}
           ref={ref}
           className={cx(styles, className)}
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+            onValueChanging?.(e.target.value);
+          }}
+          onFocus={(event) => {
+            onFocus?.(event);
+          }}
+          onBlur={(event) => {
+            triggerUpdate();
+            onBlur?.(event);
+          }}
           onPressEnter={(e) => {
             if (compositionRef.current) return;
+            triggerUpdate();
             props.onPressEnter?.(e);
           }}
           onCompositionStart={(e) => {
--- src/antd/InputNumber.tsx
+++ src/antd/InputNumber.tsx
@@ -1,6 +1,7 @@
 import type { InputNumberProps as Props } from 'antd';
 import { InputNumber as _InputNumber } from 'antd';
 import type { FC } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { ConfigProvider } from '../ConfigProvider';
 import { createStyles } from '../theme';
@@ -19,16 +20,70 @@ const useStyles = createStyles(
     `,
 );
 
-export type InputNumberProps = Props;
+export interface InputNumberProps extends Omit<Props, 'value' | 'defaultValue'> {
+  /**
+   * 输入框值变化前回调函数
+   * @param value - 输入框的值
+   */
+  onValueChanging?: (value: number) => void;
+  /**
+   * 输入框的值
+   */
+  value?: number;
+  defaultValue?: number;
+}
 
-export const InputNumber: FC<InputNumberProps> = ({ className, ...props }) => {
+export const InputNumber: FC<InputNumberProps> = ({
+  className,
+  value,
+  onValueChanging,
+  onChange,
+  defaultValue,
+  onFocus,
+  onBlur,
+  onPressEnter,
+  ...props
+}) => {
   const { styles, cx } = useStyles();
+
+  const [input, setInput] = useState<number>(defaultValue || value);
+  const valueRef = useRef<number>(defaultValue);
+
+  useEffect(() => {
+    if (typeof value !== 'undefined') {
+      setInput(value);
+    }
+  }, [value]);
+
+  const triggerUpdate = () => {
+    if (input !== valueRef.current) {
+      onChange?.(input);
+      valueRef.current = input;
+    }
+  };
 
   return (
     <ConfigProvider>
       <_InputNumber
         size={'small'}
         {...props}
+        value={input}
+        onFocus={(event) => {
+          onFocus?.(event);
+        }}
+        onBlur={(event) => {
+          triggerUpdate();
+          onBlur?.(event);
+        }}
+        onChange={(v: number) => {
+          setInput(v);
+          onValueChanging?.(v);
+        }}
+        onPressEnter={(e) => {
+          triggerUpdate();
+
+          onPressEnter?.(e);
+        }}
         className={cx(styles, className)}
       />
     </ConfigProvider>
