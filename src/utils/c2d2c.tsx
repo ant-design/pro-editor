ea4ad310eb97ee405755d2937defdd9945f02b49
大猫兼续 <rdmclin2@gmail.com>
2023-08-01 14:47:04 +0800

    :sparkles: feat: add c2d2c getDiffPropsWithSchema and getSymbolMasterNameFromProps func (#52)

--- src/utils/c2d2c.tsx
+++ src/utils/c2d2c.tsx
@@ -1,6 +1,9 @@
 import { JSONSchema } from '@/types/schema';
+import isArray from 'lodash.isarray';
 import isEmpty from 'lodash.isempty';
+import isEqual from 'lodash.isequal';
 import isNil from 'lodash.isnil';
+import isObject from 'lodash.isobject';
 import omitBy from 'lodash.omitby';
 import uniq from 'lodash.uniq';
 import { ReactNodeElement } from '../types';
@@ -18,6 +21,79 @@ export const getDefaultValueFromSchema = (schema: JSONSchema) => {
   }
   if (schema.type === 'null') return null;
   return schema.default;
+};
+
+/**
+ * 获取去重后的 props
+ * @param props
+ * @param schema
+ */
+export const getDiffPropsWithSchema = (props: any, schema?: JSONSchema) => {
+  // 如果没有 schema，则使用原来的 props
+  if (!schema) return props;
+
+  const defaultProps = getDefaultValueFromSchema(schema);
+
+  if (!defaultProps) return props;
+
+  const filtered = Object.entries(props)
+    // 过滤掉默认值
+    .filter((entry) => {
+      const [key, value] = entry;
+
+      const defaultPropsValue = defaultProps[key];
+
+      // 如果该属性在默认值中不存在
+      if (typeof defaultProps[key] === 'undefined') return true;
+
+      // 或者与默认值不相等
+      return !isEqual(defaultPropsValue, value);
+    });
+
+  return Object.fromEntries(filtered);
+};
+
+/**
+ * 根据组件名称、props 生成 props 和 schema生成symbolMaster名称
+ */
+export const getSymbolMasterNameFromProps = (
+  pkg: string,
+  component: string,
+  props: any,
+  schema?: JSONSchema,
+) => {
+  const validProps = getDiffPropsWithSchema(props, schema);
+
+  // 用一个递归方法来层层结构生成对象名称
+  const genName = (propsObj: object, parentKey?: string): string => {
+    return Object.entries(propsObj)
+      .map((entry) => {
+        const [key, value] = entry;
+
+        // 针对数组，需要结构它的内部对象
+        if (isArray(value)) {
+          return `${key}=[${value.map((item) => `{${genName(item)}}`).join(',')}]`;
+        }
+
+        // 嵌套对象 递归解析
+        if (isObject(value)) {
+          return genName(value, key);
+        }
+
+        // undefined 和 null 直接过滤
+        if (isNil(value)) {
+          return '';
+        }
+        // 普通的其他值直接返回
+        return `${parentKey ? `${parentKey}.` : ''}${key}=${value}`;
+      })
+      .filter((i) => i)
+      .join(',');
+  };
+
+  const propsStr = genName(validProps);
+
+  return `${pkg.replace('/', '-')}/${component}/${propsStr}`;
 };
 
 /**
