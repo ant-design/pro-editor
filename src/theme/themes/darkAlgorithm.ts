0a8f89508f2d5a034a96655ff782643f07d706bd
rdmclin2 <rdmclin2@gmail.com>
2023-08-01 19:37:00 +0800

    :tada: fix: MappingAlgorithm type position

--- src/theme/themes/darkAlgorithm.ts
+++ src/theme/themes/darkAlgorithm.ts
@@ -1,11 +1,12 @@
 import { theme } from 'antd';
+import type { MappingAlgorithm } from 'antd/es/theme/interface';
 
 /**
  * studio 暗色模式下算法
  * @param seedToken
  * @param mapToken
  */
-export const studioDarkAlgorithm = (seedToken, mapToken) => {
+export const studioDarkAlgorithm: MappingAlgorithm = (seedToken, mapToken) => {
   const mergeToken = theme.darkAlgorithm(seedToken, mapToken);
 
   return {
