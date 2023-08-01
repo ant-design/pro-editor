97e71bc0f470dce064d044434b4dde11111e6510
rdmclin2 <rdmclin2@gmail.com>
2023-08-01 18:50:40 +0800

    :bug: fix: MappingAlgorithm no longer exported

--- src/theme/themes/darkAlgorithm.ts
+++ src/theme/themes/darkAlgorithm.ts
@@ -1,12 +1,11 @@
 import { theme } from 'antd';
-import type { MappingAlgorithm } from 'antd/es/config-provider/context';
 
 /**
  * studio 暗色模式下算法
  * @param seedToken
  * @param mapToken
  */
-export const studioDarkAlgorithm: MappingAlgorithm = (seedToken, mapToken) => {
+export const studioDarkAlgorithm = (seedToken, mapToken) => {
   const mergeToken = theme.darkAlgorithm(seedToken, mapToken);
 
   return {
