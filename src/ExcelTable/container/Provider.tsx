51953b08baf17334b7cdc0efb6bf3fb63ec5f403
arvinxx <arvinx@foxmail.com>
2023-08-11 11:41:35 +0800

    :fire: refactor: 移除 ExcelTable 组件

--- src/ExcelTable/container/Provider.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC, ReactNode } from 'react';
-
-import { createStore, Provider } from '../store';
-
-const StoreProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  return <Provider createStore={createStore}>{children}</Provider>;
-};
-
-export default StoreProvider;
