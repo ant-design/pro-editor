2382773533acc6f86dbd2ae35054d91ee5d9e52c
rdmclin2 <rdmclin2@gmail.com>
2024-01-02 17:22:41 +0800

    :twisted_rightwards_arrows: chore: merge remote

--- src/SortableTree/features/TreeList.tsx
+++ src/SortableTree/features/TreeList.tsx
@@ -79,6 +79,7 @@ const TreeList = memo(() => {
   const { styles } = useStyle();
 
   const { height = 800, itemHeight = () => 36, width = '100%' } = virtual || {};
+  console.log('flattenData', flattenData);
 
   return (
     <>
