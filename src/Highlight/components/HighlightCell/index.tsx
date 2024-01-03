6553254dd92a1d5ca28152a5cc3b03821367b8b7
rdmclin2 <rdmclin2@gmail.com>
2024-01-03 09:57:43 +0800

    :bug: fix: lint error

--- src/Highlight/components/HighlightCell/index.tsx
+++ src/Highlight/components/HighlightCell/index.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { useStyles } from './style';
 
 export interface HighlightCellProps {
+  prefixCls: string;
   data: { index: number; value: string };
   emptyText?: string;
   onMouseDown?: React.MouseEventHandler;
