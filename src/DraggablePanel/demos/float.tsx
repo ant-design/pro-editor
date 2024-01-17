b93960450ff30ce2c0ab20bb6dce58ae49ac72d6
rdmclin2 <rdmclin2@gmail.com>
2024-01-17 13:48:35 +0800

    :bug: fix: context menu style

--- src/DraggablePanel/demos/float.tsx
+++ src/DraggablePanel/demos/float.tsx
@@ -3,18 +3,17 @@
  */
 import { DraggablePanel } from '@ant-design/pro-editor';
 
-export default () => (
-  <div
-    style={{
-      background: '#f1f1f1',
-      border: '2px solid #ddd',
-      height: 500,
-      display: 'flex',
-    }}
-  >
-    <div style={{ flex: 1, padding: 12 }}>内容</div>
-    <DraggablePanel style={{ background: '#fff', padding: 12 }} mode={'float'}>
-      悬浮可拖拽面板
-    </DraggablePanel>
-  </div>
-);
+export default () => {
+  return (
+    <div
+      style={{
+        height: 500,
+        display: 'flex',
+      }}
+    >
+      <DraggablePanel style={{ padding: 12 }} mode={'float'}>
+        悬浮可拖拽面板
+      </DraggablePanel>
+    </div>
+  );
+};
