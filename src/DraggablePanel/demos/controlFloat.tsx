d3b96457bb72d155130c95b345dbde1c0435a4e4
rdmclin2 <rdmclin2@gmail.com>
2024-01-17 13:48:35 +0800

    :bug: fix: context menu style

--- src/DraggablePanel/demos/controlFloat.tsx
+++ src/DraggablePanel/demos/controlFloat.tsx
@@ -11,18 +11,15 @@ export default () => {
   return (
     <div
       style={{
-        background: '#f1f1f1',
-        border: '2px solid #ddd',
         height: 500,
         display: 'flex',
       }}
     >
-      <div style={{ flex: 1, padding: 12 }}>内容</div>
       <DraggablePanel
         position={position}
         onPositionChange={setPos}
         mode={'float'}
-        style={{ background: '#fff', padding: 12 }}
+        style={{ padding: 12 }}
       >
         悬浮可拖拽面板
       </DraggablePanel>
--- src/DraggablePanel/demos/left.tsx
+++ src/DraggablePanel/demos/left.tsx
@@ -1,16 +1,12 @@
 /**
- * height: 600
+ * compact: true
  */
 import { DraggablePanel } from '@ant-design/pro-editor';
 import { Flexbox } from 'react-layout-kit';
 
 export default () => (
-  <Flexbox horizontal style={{ background: '#f1f1f1', border: '2px solid #ddd', height: 300 }}>
-    <DraggablePanel
-      placement={'left'}
-      maxWidth={800}
-      style={{ background: '#fff', width: '100%', padding: 12 }}
-    >
+  <Flexbox horizontal style={{ height: 300 }}>
+    <DraggablePanel placement={'left'} maxWidth={800} style={{ width: '100%', padding: 12 }}>
       左侧面板
     </DraggablePanel>
     <div style={{ flex: 1, padding: 12 }}>内容</div>
