f7f42e4ebf8fa853fb7d9e19136a17fcb71c54f4
rdmclin2 <rdmclin2@gmail.com>
2024-01-25 18:02:19 +0800

    :bug: fix: ts error

--- src/DataPreviewer/demos/showType.tsx
+++ src/DataPreviewer/demos/showType.tsx
@@ -2,4 +2,4 @@ import { DataPreviewer } from '@ant-design/pro-editor';
 
 import { data } from './data';
 
-export default () => <DataPreviewer data={data} showType={false} />;
+export default () => <DataPreviewer data={data} />;
